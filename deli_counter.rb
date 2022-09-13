0a907fff1b630e66b138af0c9a3fe4ffa0204bde
Yejin Na <101006019+JustEddie@users.noreply.github.com>
2022-09-13 23:45:11 +1000

    commit tree c11cae2ccd76e6c48871a58888e076146934b84f

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,26 @@
 # Write your code here.
+def line(array)
+    if array.size == 0
+        puts "The line is currently empty."
+    else
+        result = "The line is currently:"
+        array.each_with_index {|name,index| result << " #{index+1}. #{name}"}
+        puts result
+    end
+end
+
+def take_a_number(array, name)
+    array.push(name)
+    result = ""
+    array.each_with_index {|name, index| result << " #{index+1}. #{name}"}
+    puts "Welcome, #{name}. You are number #{array.size} in line."
+end
+
+def now_serving(array)
+    if array.size == 0
+        puts "There is nobody waiting to be served!"
+    else
+        puts "Currently serving #{array[0]}."
+        array.shift
+    end
+end
